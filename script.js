86a33050e9d4c4290aa4aa8a8cce47aebf76cf83
tani0422 <tanisthamehta@gmail.com>
2026-02-03 20:32:29 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+const message = document.getElementById("message");
+
+yesBtn.addEventListener("click", () => {
+  message.innerHTML = "YAY!!! 💖 I knew you'd say yes 😍";
+  document.body.style.background = "#ffe0ec";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
